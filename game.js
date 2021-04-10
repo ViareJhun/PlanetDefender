d11b6170d0c0dda5a9e22b4277562f1b5019c7c8
ViareJhun <81759911+ViareJhun@users.noreply.github.com>
2021-04-11 01:16:40 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -66,6 +66,8 @@ function loadTextures()
 	tex_path['planet'] = 'img/planet.png';
 	tex_path['back1'] = 'img/back1.png';
 	tex_path['asteroid'] = 'img/asteroid.png';
+	tex_path['restart0'] = 'img/restart0.png';
+	tex_path['restart1'] = 'img/restart1.png';
 	
 	Object.keys(tex_path).forEach(
 		(key) =>
@@ -148,6 +150,8 @@ addEventListener(
 		if (e.which == 1)
 		{
 			mouse_check = 0;
+			
+			mouseUp();
 		}
 	}
 );
@@ -177,12 +181,17 @@ addEventListener(
 	function (e)
 	{
 		mouse_check = 0;
+		
+		mouseUp();
 	}
 )
 
 // Game
 var game_state = 'load';
 
+var version = 1;
+
+var lives = 2;
 var asteroids = [];
 var defender = [];
 var def_radius = 80;
@@ -200,6 +209,31 @@ var as_time_max = 60;
 var as_time = as_time_max;
 var as_speed = 1.5;
 var hits = 0;
+var score = 0;
+var max_score = 0;
+var timer_max = 60;
+var timer = timer_max;
+var score_qual = 1;
+
+var T1 = Math.random() * d360;
+var T2 = Math.random() * d360;
+
+function preLoad()
+{
+	max_score = sessionStorage.getItem('dioScore' + version);
+	if (max_score == null)
+	{
+		max_score = 0;
+	}
+}
+
+function clearObjects()
+{
+	defender = [];
+	asteroids = [];
+	
+	lives = 2;
+}
 
 function startGame()
 {
@@ -211,8 +245,75 @@ function startGame()
 	);
 	
 	game_state = 'game';
+	
+	score = 0;
+	score_qual = 1;
+	score_draw = 0;
+	
+	as_speed = 1.5;
+	as_time_max = 60;
 }
 
+// lose
+var restart_time = 60 * 3;
+var restart_alpha = 0;
+var restart_scale = 1;
+var restart_to = 1;
+var restart_x = surface.width * 0.5;
+var restart_y = surface.height * 0.6;
+var score_draw = 0;
+var to_menu = 0;
+
+function loseGame()
+{
+	if (score > max_score)
+	{
+		max_score = score;
+	}
+	
+	restart_to = 1;
+	restart_scale = 1;
+	to_menu = 0;
+	
+	restart_time = 60 * 3;
+	restart_alpha = 0;
+	score_draw = 0;
+	
+	clearObjects();
+	
+	sessionStorage.setItem('dioScore' + version, max_score);
+	
+	game_state = 'lose';
+}
+
+
+function mouseUp()
+{
+	switch (game_state)
+	{
+		case 'lose':
+		{
+			if (restart_time <= 0)
+			{
+				if (
+					distance(
+						mouse_x,
+						mouse_y,
+						restart_x,
+						restart_y
+					) < 128
+				)
+				{
+					to_menu = 1;
+				}
+			}
+		}
+		break;
+	}
+}
+
+
+// Game Objects
 function CreateDefender(side)
 {
 	this.x = surface.width * 0.5;
@@ -291,7 +392,7 @@ function CreateAsteroid(x, y, dir)
 			) < this.radius + planet.radius
 		)
 		{
-			hits ++;
+			lives --;
 			return 1;
 		}
 		
@@ -350,6 +451,17 @@ function gameUpdate()
 	defender[0].update();
 	defender[1].update();
 	
+	if (timer > 0)
+	{
+		timer --;
+	}
+	else
+	{
+		timer = timer_max;
+		
+		score += score_qual;
+	}
+	
 	// input
 	if (mouse_check)
 	{
@@ -364,11 +476,11 @@ function gameUpdate()
 		
 		if (mouse_x > planet.x)
 		{
-			to_angle -= as_speed * d1 * 1;
+			to_angle -= as_speed * d1 * 1.2;
 		}
 		else
 		{
-			to_angle += as_speed * d1 * 1;
+			to_angle += as_speed * d1 * 1.2;
 		}
 	}
 	
@@ -398,13 +510,17 @@ function gameUpdate()
 	}
 	else if (as_speed < 4)
 	{
+		score_qual = 5;
+		
 		as_speed += 0.0001;
 		
 		as_time_max = Math.max(as_time_max - 0.01, 30);
 	}
 	else
 	{
-		as_time_max = Math.min(as_time_max + 0.05, 50);
+		score_qual = 10;
+		
+		as_time_max = Math.min(as_time_max + 0.05, 37);
 		as_speed += 0.00005;
 	}
 	
@@ -431,7 +547,7 @@ function gameUpdate()
 	else
 	{
 		let side = choose([-1, 1]);
-		let len = surface.height * 0.5;
+		let len = surface.height * 0.7;
 		
 		let angle = side * d90 + Math.random() * d45 * 1.15 * choose([-1, 1]);
 		
@@ -453,6 +569,16 @@ function gameUpdate()
 // Loop (upd + draw)
 function loop()
 {
+	T1 += 0.02;
+	T2 += 0.1;
+	
+	// Clear
+	context.drawImage(
+		tex['back1'],
+		0,
+		0
+	);
+	
 	switch (game_state)
 	{
 		case 'load':
@@ -464,13 +590,6 @@ function loop()
 		case 'game':
 		{
 			gameUpdate();
-			
-			// Clear
-			context.drawImage(
-				tex['back1'],
-				0,
-				0
-			);
 			
 			// Planet
 			planet.angle += 0.01;
@@ -500,22 +619,149 @@ function loop()
 			);
 			
 			// GUI
+			if (score_draw < score)
+			{
+				score_draw += Math.max(1, Math.round(score * 0.02));
+			}
+			else
+			{
+				score_draw = score;
+			}
+			
 			context.font = '20px monospace';
+			context.fillStyle = '#FFFFFF';
+			
+			context.textAlign = 'left';
+			context.textBaseline = 'top';
+			
 			context.fillText(
-				as_speed,
+				'Счёт:' + score_draw + ' Рекорд:' + max_score,
 				8,
-				16
+				8
 			);
 			context.fillText(
-				as_time_max,
+				'Жизней:' + lives,
 				8,
 				32
 			);
-			context.fillText(
-				'Попаданий в планету:' + hits,
-				8,
-				48
+			
+			if (lives <= 0)
+			{
+				loseGame();
+			}
+		}
+		break;
+		
+		case 'lose':
+		{
+			// GUI
+			
+			context.fillStyle = '#FFFFFF';
+			context.font = '25px monospace';
+			
+			context.textAlign = 'center';
+			context.textBaseline = 'middle';
+			
+			context.save();
+			
+			context.translate(
+				surface.width * 0.5,
+				surface.height * 0.35
 			);
+			
+			context.rotate(
+				Math.sin(T1 * 0.5) * d1 * 15
+			);
+			
+			context.fillText(
+				'Вы проиграли!',
+				0,
+				-12
+			);
+			
+			if (score_draw < score)
+			{
+				score_draw += Math.max(1, Math.round(score * 0.02));
+			}
+			else
+			{
+				score_draw = score;
+			}
+			
+			context.font = '15px monospace';
+			
+			context.fillText(
+				'Вы набрали:' + score_draw + '!',
+				0,
+				12
+			);
+			
+			context.restore();
+			
+			// button
+			if (restart_time > 0)
+			{
+				restart_time --;
+			}
+			else
+			{
+				restart_alpha = Math.min(
+					1,
+					restart_alpha += 0.02
+				);
+				
+				context.globalAlpha = restart_alpha;
+				
+				let color = 0;
+				
+				restart_to = 1;
+				
+				if (mouse_check)
+				{
+					if (
+						distance(
+							mouse_x,
+							mouse_y,
+							restart_x,
+							restart_y
+						) < 128
+					)
+					{
+						restart_to = 0.75;
+						color = 1;
+					}
+				}
+				
+				restart_scale += (
+					restart_to - restart_scale
+				) * 0.1;
+				
+				if (restart_scale > 0.9)
+				{
+					if (to_menu)
+					{
+						startGame();
+					}
+				}
+				
+				context.save();
+				context.translate(
+					restart_x,
+					restart_y
+				);
+				context.scale(
+					restart_scale,
+					restart_scale
+				);
+				context.drawImage(
+					tex['restart' + color],
+					-64,
+					-64
+				);
+				context.restore();
+				
+				context.globalAlpha = 1.0;
+			}
 		}
 		break;
 	}
@@ -526,5 +772,8 @@ function loop()
 // Start Game
 loadTextures();
 setScreen();
+preLoad();
+
+loseGame();
 
 requestAnimationFrame(loop);
